1b3e96da08c856a358cbf8614038207c801f4295
donxiya <31514669+donxiya@users.noreply.github.com>
2020-05-22 08:12:36 -0500

    remove legacy

--- src/pages/RoleDisplay.tsx
+++ src/pages/RoleDisplay.tsx
@@ -1,18 +1,12 @@
 import React, { useState } from 'react';
 import RoleDisplay from '../components/RoleDisplay/RoleDisplayContainer'
 import "../style/roleDisplay.scss";
-import { User } from '../dtos/user';
-import { Hidden } from '@material-ui/core';
-import { Redirect } from 'react-router-dom';
+
 
 
 function RoleDisplayPage() {
-    let loading = async function () {
 
-    }
-    const [loadingFunction] = useState(() => {
-        return loading
-    })
+
     return (
         <>
             <div className="outer-frame neon">
