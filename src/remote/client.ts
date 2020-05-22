ad9263ac479df5029a2177ce728015d07a42b094
donxiya <31514669+donxiya@users.noreply.github.com>
2020-05-21 23:34:42 -0500

    discard

--- src/remote/client.ts
+++ src/remote/client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const Client = axios.create({
-    //baseURL: 'http://haotranp1-env.eba-c3uvcxtp.us-east-2.elasticbeanstalk.com',
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'http://haotranp1-env.eba-c3uvcxtp.us-east-2.elasticbeanstalk.com',
+    //baseURL: 'http://localhost:8080/',
     headers: {
         'Content-Type': 'application/json'
     }
